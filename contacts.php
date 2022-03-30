97302bca0f9bef339383238ab2be24252305110b
ruturajnawale10 <ruturajnawale99@gmail.com>
2022-03-29 17:36:05 -0700

    website shows contacts list using login creds

---
+++ contacts.php
@@ -0,0 +1,31 @@
+<!Doctype html>
+<html>
+    
+<link rel="stylesheet" href="style.css">
+
+<div class="topnav">
+  <a href="index.php">Home</a>
+  <a href="about.html">About</a>
+  <a href="products.html">Products</a>
+  <a href="news.html">News</a>
+  <a class="active" href="contacts.php">Contact</a>
+</div>
+
+<?php
+
+$file = fopen("Contacts.txt","r");
+$listPhones = array();
+
+while(! feof($file))
+  {
+    array_push($listPhones, fgets($file));
+  }
+fclose($file);
+
+echo '<ul>';
+    echo '<li>' . implode( '</li><li>', $listPhones) . '</li>';
+echo '</ul>';
+
+?>
+
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<html>
+<link rel="stylesheet" href="style.css">
+
+<title>Paintings collection</title>
+<h1 style="color: blue">Paintings collection</h2>
+
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="about.html">About</a>
+  <a href="about.html">Products</a>
+  <a href="news.html">News</a>
+  <a href="login.html">Contact</a>
+  
+</div>
+
+<img id = "img1" src="images/home_img.jpg"/>
+
+<h1>Buy Original Art Online on our Art Gallery</h1>
+</html>
